6bf1abc59640c35bb16f1ca1a428fdb32b4bb48f
JOHANNA REXIN <johanna.rexin@gmail.com>
2021-02-04 15:02:54 +0100

    Set up database

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import mongoose from 'mongoose'
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
+import ceramics from './ceramics.data.json'
+
 const port = process.env.PORT || 8080
 const app = express()
 
@@ -13,9 +12,35 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/final-project"
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.Promise = Promise
+
+const Ceramics = mongoose.model('Ceramics', {
+  header: String,
+  text: String,
+  // image: "" // VAD SKA EN BILD HA?
+})
+
+if (process.env.RESET_DATABASE) {
+  const seedDatabase = async () => {
+    await Ceramics.deleteMany({})
+
+    ceramics.forEach((item) => {
+      const newCeramic = new Ceramics(item)
+      newCeramic.save()
+
+    })
+  }
+  seedDatabase()
+}
+
+new Ceramics({ header: 'Hej', text: 'En fin kruka'}).save()
+
+
 // Start defining your routes here
 app.get('/', (req, res) => {
-  res.send('Hello world hej hej')
+  res.send('This is an API with my ceramics. Best, Johanna Rexin')
 })
 
 // Start the server
